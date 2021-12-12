ac37e9fa5ca45ce030500a5cf5b106f48d9a9e01
Evelyn <udderlyevelyn@gmail.com>
2021-12-12 14:15:17 -0500

    Changed wrong value before, lol - right one this time.

--- 1.3/Source/RebeccaRealistic/RebeccaSettings.cs
+++ 1.3/Source/RebeccaRealistic/RebeccaSettings.cs
@@ -20,9 +20,9 @@ namespace RR
         public static float VisitorIsOrbitalChance = .46f;
         public static int VisitorMinimumSpacingTicks = 1250;
         public static int VisitorMaximumSpacingTicks = 2500;
-        public static float ThreatPointsMultiplier = 3.5f;
+        public static float ThreatPointsMultiplier = 3f;
         public static float MTBUnit = 40000f;
-        public static float HighThreatRarityExponent = 2;
+        public static float HighThreatRarityExponent = 3.5f;
         public static bool LoggingEnabled = false;
 
         public override void ExposeData()
